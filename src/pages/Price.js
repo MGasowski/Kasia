df729db1453ff1d06187dd9a12ba83ad1c25a102
MGasowski <gasowski0marcin@gmail.com>
2021-10-12 16:58:44 +0200

    Landing page layout

---
+++ src/pages/Price.js
@@ -0,0 +1,102 @@
+const Price = () => {
+    return (
+        <div class="container">
+           
+    <section class="slice slice-lg">
+      <div class="container">
+        <div class="mb-5 text-center">
+          <span class="badge badge-soft-success badge-pill badge-lg">
+            Cennik
+          </span>
+          <h3 class=" mt-4">Cennik</h3>
+          <div class="fluid-paragraph mt-3">
+            <p class="lead lh-180">Podany na stronie cennik zawiera ceny badań
+                podstawowych i pakietów. W celu uzyskania
+                informacji o cenach badań specjalistycznych
+                prosimy o kontakt telefoniczny 692332349.</p>
+          </div>
+        </div>
+        
+        </div>
+    </section>
+
+        <div class="list-group">
+                        <div class="list-group-item active">
+                <div class="media align-items-center">
+ 
+                    <div class="media-body">
+                        <h6 class="text-sm d-block text-limit mb-0">Purpose Design System</h6>
+                        <span class="d-block text-sm text-muted">Development</span>
+                    </div>
+                    <div class="media-body text-right">
+                        <span class="text-sm text-dark font-weight-bold ml-3">
+                            $2500
+                        </span>
+                    </div>
+                </div>
+
+            </div>
+            <div class="list-group-item">
+                <div class="media align-items-center">
+ 
+                    <div class="media-body">
+                        <h6 class="text-sm d-block text-limit mb-0">Purpose Design System</h6>
+                        <span class="d-block text-sm text-muted">Development</span>
+                    </div>
+                    <div class="media-body text-right">
+                        <span class="text-sm text-dark font-weight-bold ml-3">
+                            $2500
+                        </span>
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="list-group-item">
+
+                <div class="media align-items-center">
+                    <div class="mr-3">
+
+                        <img alt="Image placeholder" src="../../assets/img/theme/light/brand-avatar-2.png" class="avatar  rounded-circle" />
+
+                    </div>
+                    <div class="media-body">
+                        <h6 class="text-sm d-block text-limit mb-0">Website redesign</h6>
+                        <span class="d-block text-sm text-muted">Identity</span>
+                    </div>
+                    <div class="media-body text-right">
+                        <span class="text-sm text-dark font-weight-bold ml-3">
+                            $1800
+                        </span>
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="list-group-item">
+
+                <div class="media align-items-center">
+                    <div class="mr-3">
+
+                        <img alt="Image placeholder" src="../../assets/img/theme/light/brand-avatar-3.png" class="avatar  rounded-circle" />
+
+                    </div>
+                    <div class="media-body">
+                        <h6 class="text-sm d-block text-limit mb-0">Webpixels website launch</h6>
+                        <span class="d-block text-sm text-muted">Branding</span>
+                    </div>
+                    <div class="media-body text-right">
+                        <span class="text-sm text-dark font-weight-bold ml-3">
+                            $3150
+                        </span>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+        </div>
+    );
+}
+
+export default Price;
